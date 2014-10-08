87239b8bd7023258312486c8440fc762b47ed8e6
Marco Dalla G <marco.dallagiacoma@gmail.com>
2014-10-08 11:58:11 +0200

    added support for totals row in pdf generation

--- tablib/formats/_pdf.py
+++ tablib/formats/_pdf.py
@@ -17,15 +17,25 @@ table_style = [
     ('INNERGRID', (0, 0), (-1, -1), 0.25, colors.black),
     ('BOX', (0, 0), (-1, -1), 0.25, colors.black),
     ('ROWBACKGROUNDS', (0, 0), (-1, -1), [colors.white, colors.Whiter(colors.black, 0.1)]),
+    ('FACE', (0, 0), (-1, -1), 'Helvetica'),
 ]
 
 header_style = [
     ('BACKGROUND', (0, 0), (-1, 0), colors.Whiter(colors.black, 0.6)),
-    ('TEXTCOLOR', (0, 0), (-1, 0), colors.white)
+    ('TEXTCOLOR', (0, 0), (-1, 0), colors.white),
+    ('FACE', (0, 0), (-1, 0), 'Helvetica-Bold'),
+]
+
+totals_style = [
+    ('FACE', (0, -1), (-1, -1), 'Helvetica-Bold'),
 ]
 
 title_style = ParagraphStyle(
     name='Title Style', fontName='Helvetica-Bold', fontSize=16
+)
+
+description_style = ParagraphStyle(
+    name='Description Style', fontName='Helvetica-Bold', fontSize=10
 )
 
 
@@ -36,34 +46,44 @@ def export_book(databook):
 
     for dataset in databook._datasets:
         data = []
+        totals = None
 
         if dataset.headers is not None:
             new_header = [item if item is not None else '' for item in
                           dataset.headers]
             data.append(new_header)
 
-        for row in dataset:
+        for row in dataset._data:
             new_row = [item if item is not None else '' for item in row]
-            data.append(new_row)
+            if row.has_tag('totals'):
+                totals = row
+            else:
+                data.append(new_row)
+
+        if totals is not None:
+            data.append(totals)
 
         if not data:
             continue
 
-        t = Table(data, hAlign='LEFT')
-        # noinspection PyTypeChecker
-        t.setStyle(
-            (table_style + header_style) if dataset.headers is not None
-            else table_style
-        )
+        table = Table(data, hAlign='LEFT')
+
+        style = table_style[:]
+        if totals is not None:
+            style += totals_style
+        if dataset.headers:
+            style += header_style
+
+        table.setStyle(style)
 
         if dataset.title is not None:
             elements.append(Paragraph(dataset.title, title_style))
             elements.append(Spacer(1, 20))
 
-        elements.append(t)
+        elements.append(table)
         elements.append(PageBreak())
 
-        max_width = max(max_width, t.minWidth() + 2*margin + 2*dataset.width)
+        max_width = max(max_width, table.minWidth() + 2*margin + 2*dataset.width)
 
     if max_width > A4[0]:
         pagesize[0] = max_width
